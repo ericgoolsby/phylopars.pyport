53d37605ebf465c97861e02528f76e63557ebf6b
Eric Goolsby <eric.goolsby.evolution@gmail.com>
2026-07-02 20:53:32 -0700

    v0.1.1

--- R/data.R
+++ R/data.R
@@ -1,4 +1,4 @@
-#' Example phylogenetic tree
+#' Example 25-species phylogenetic tree
 #'
 #' A phylogenetic tree with 25 species for demonstrating phylopars functions.
 #'
@@ -9,7 +9,7 @@
 "example_tree"
 
 
-#' Example trait data
+#' Example 25-species trait data
 #'
 #' Trait data for 25 species with 4 continuous traits. Includes missing values
 #' and multiple observations per species (3 replicates each).
@@ -27,3 +27,36 @@
 #' data(example_traits)
 #' head(example_traits)
 "example_traits"
+
+#' Example 50-species phylogenetic tree
+#'
+#' A phylogenetic tree with 50 species for demonstrating phylopars functions.
+#'
+#' @format A phylo object with 50 tips
+#' @examples
+#' data(tree)
+#' plot(tree)
+"tree"
+
+#' Example trait data
+#'
+#' Trait data for 50 species with 4 continuous traits. Includes missing values
+#' and multiple observations per species (3 replicates each). This dataset has a 
+#' true interspecific trait covariance of diag(c(1,10,10,1)), a true intraspecific
+#' trait covariance of diag(c(1,1,1,1)), and a true root value of c(0,0,0,0). Useful
+#' for diagnosing convergence on the correct values (e.g., trait covariance estimates
+#' should differ from the true variances by no more than 2 in either direction).
+#'
+#' @format A data frame with 75 rows (25 species x 3 replicates) and 5 columns:
+#' \describe{
+#'   \item{species}{Species name}
+#'   \item{V1}{First trait}
+#'   \item{V2}{Second trait}
+#'   \item{V3}{Third trait}
+#'   \item{V4}{Fourth trait}
+#' }
+#'
+#' @examples
+#' data(Y)
+#' head(Y)
+"Y"
